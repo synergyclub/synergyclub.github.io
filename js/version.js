a9a95dbf2f0af23ab60d25e790021a1313be7179
Synergy Club <shhssynergy@gmail.com>
2025-05-13 10:51:20 -0400

    Added banner

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
-const SYNERGY_VERSION = "1.1.0";
+const SYNERGY_VERSION = "1.1.1";
 
 document.addEventListener("DOMContentLoaded", function () {
   let footer = document.querySelector("footer[data-version]");
