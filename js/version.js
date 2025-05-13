fe046ac55973661e86a0b057aaad7470225aaf44
Synergy Club <shhssynergy@gmail.com>
2025-05-13 12:42:16 -0400

    OOPS

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
-const SYNERGY_VERSION = "1.1.1";
+const SYNERGY_VERSION = "1.2.0";
 
 document.addEventListener("DOMContentLoaded", function () {
   let footer = document.querySelector("footer[data-version]");
