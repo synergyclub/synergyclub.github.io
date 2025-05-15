75020c702a974705ab4e55ea7fb5435af2fd4454
abtblk <137126638+abtblk@users.noreply.github.com>
2025-05-15 18:57:33 -0400

    Modified logo for mobile

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
-const SYNERGY_VERSION = "1.3.1";
+const SYNERGY_VERSION = "1.3.2";
 
 document.addEventListener("DOMContentLoaded", function () {
   let footer = document.querySelector("footer[data-version]");
