4d7036a260d6762dbadea6ec8f0d1707c2d305f2
Cocaine <137126638+abtblk@users.noreply.github.com>
2025-05-16 17:08:54 -0400

    Ugh version number update

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
-const SYNERGY_VERSION = "1.3.4";
+const SYNERGY_VERSION = "1.3.5";
 
 document.addEventListener("DOMContentLoaded", function () {
   let footer = document.querySelector("footer[data-version]");
